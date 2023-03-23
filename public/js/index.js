441336761571d7f48178933a5ea0e1ee7ebab7d9
edwinnambaje <nambajeeedwin@gmail.com>
2023-03-23 15:30:33 +0200

    first commit

---
+++ public/js/index.js
@@ -0,0 +1,10 @@
+const socket= io();
+socket.on('connect', ()=>{
+    console.log("Connected to server");
+})
+socket.on('disconnect', ()=>{
+    console.log("Disconnected from server");
+})
+socket.on('newMessage', (message)=>{
+    console.log("newMessage",message);
+})
--- server/server.js
+++ server/server.js
@@ -10,6 +10,24 @@ let server=http.createServer(app)
 let io=socketIO(server)
 io.on("connection",(socket)=>{
     console.log("A new user just connected")
+    socket.emit('newMessage',{
+        from: "admin",
+        text: "Welcome to the chat App",
+        createdAt:new Date().getTime()
+    })
+    socket.broadcast.emit('newMessage',{
+        from: "admin",
+        text: "New User joined the chat",
+        createdAt:new Date().getTime()
+    })
+    socket.on('createMessage', (message)=>{
+        console.log('createMessage',message);
+        io.emit('newMessage',{
+            from:message.from,
+            text:message.text,
+            createdAt:new Date().getTime()
+        })
+    })
     socket.on('disconnect', ()=>{
         console.log("User Disconnected from server");
     })
